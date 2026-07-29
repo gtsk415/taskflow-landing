1a334bcc1f0b16dbbf413cb104585f2e68019f85
gtsk415 <print.bistro@mail.ru>
2026-07-29 15:09:42 +0300

    fix: use appendChild instead of innerHTML

--- board.js
+++ board.js
@@ -6,9 +6,24 @@ const toDoList = document.querySelector('.todo__list') ;
 
 
 btn.addEventListener('click', () => {
-  if(textInput.value) {
-    toDoList.innerHTML = `<li>${textInput.value}</li>`;
-  } else {
+  const text = textInput.value.trim();
+
+  if (!text) {
     console.log('empty input');
+    return;
   }
+
+  const li = document.createElement('li');
+
+  const span = document.createElement('span');
+  span.textContent = text;
+
+  const delBtn = document.createElement('button');
+  delBtn.textContent = 'x'
+
+  li.appendChild(span);
+  li.appendChild(delBtn);
+  toDoList.appendChild(li);
+
+  textInput.value = '';
 });
